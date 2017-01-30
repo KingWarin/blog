2ba98d6e43334e63e50af2a4f190fe04b637b06b
KingWarin <info@kingwarin.de>
2017-01-30 21:05:14 +0100

    AdminMenu: Add create blog post entry

--- php/admin.php
+++ php/admin.php
@@ -6,6 +6,9 @@
         echo 'Not logged in';
     } else {
         echo 'Welcome '.$_SESSION['username'];
+        echo '<hr />';
+        echo '<br /><a href="new_entry.php">Create a new blog post</a>';
+        echo '<hr />';
         echo '<a href="?logout">Logout</a>';
     }
 ?>
