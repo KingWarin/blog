0d034b90ae188877c84a1c1c59cfef00dfefbc10
KingWarin <info@kingwarin.de>
2017-02-02 20:30:34 +0100

    DBConnect: Add statement functions
    Add functions for:
    - addLanguage
    - addUser
    - addComment

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -21,8 +21,41 @@
             $stmt = $this->con->prepare("INSERT INTO categories (categoryName, parentId) VALUES (:category_name, :parent_id)");
             $stmt->bindParam(':category_name', $categoryName);
             $stmt->bindParam(':parent_id', $parentId);
-            $stmt->execute();
-            var_dump($stmt);
+            $res = $stmt->execute();
+            return $res;
+        }
+
+        public function addLanguage($lang, $icon) {
+            $stmt = $this->con->prepare("INSERT INTO languages (language, langIcon) VALUES (:lang, :icon)");
+            $stmt->bindParam(':lang', $lang);
+            $stmt->bindParam(':icon', $icon);
+            $res = $stmt->execute();
+            return $res;
+        }
+
+        public function addUser($alias, $mail, $pass, $salt) {
+            $stmt = $this->con->prepare(
+                "INSERT INTO users (userAlias, userMail, userPass, status, salt) VALUES (:user, :userMail, :userPass, 'active', :userSalt)"
+            );
+            $userPass = hash('sha256', $pass . $salt);
+            $stmt->bindParam(':user', $alias);
+            $stmt->bindParam(':userMail', $mail);
+            $stmt->bindParam(':userPass', $userPass);
+            $stmt->bindParam(':userSalt', $salt);
+            $res = $stmt->execute();
+            return $res;
+        }
+
+        public function addComment($commentor, $mail, $page, $comment) {
+            $stmt = $this->con->prepare(
+                "INSERT INTO comments (createDate, commentorName, commentorMail, commentorPage, comment) VALUES (NOW(), :name, :mail, :page, :comment)"
+            );
+            $stmt->bindParam(':name', $commentor);
+            $stmt->bindParam(':mail', $mail);
+            $stmt->bindParam(':page', $page);
+            $stmt->bindParam(':comment', $comment);
+            $res = $stmt->execute();
+            return $res;
         }
     }
 ?>
