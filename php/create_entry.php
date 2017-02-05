660136c3ac6ce7287a76ff3a4f13e8c774683998
KingWarin <info@kingwarin.de>
2017-02-05 15:40:09 +0100

    CreateEntry: Add basic create functionality

--- php/create_entry.php
+++ php/create_entry.php
@@ -5,10 +5,34 @@
     if(!check_login()) {
         header('Location: ../login.html');
     } else {
-        echo 'Welcome '.$_SESSION['username'];
-        echo '<hr />';
-        echo '<br /><a href="new_entry.php">Create a new blog post</a>';
-        echo '<hr />';
-        echo '<a href="?logout">Logout</a>';
+        if(isset($_POST['articleHeading'], $_POST['contentHeading'], $_POST['language'], $_POST['content'])) {
+            $status = 'draft';
+            if(isset($_POST['publish'])) {
+                $status = 'published';
+            }
+            $connection = new Connection();
+            $article = $connection->createArticle($_POST['articleHeading'], $status);
+            if(!$article) {
+                //an error occured, do some error handling
+                echo "Unable to create article";
+            } else {
+                $content = $connection->createContentForArticle($article, $_POST['contentHeading'], $_POST['content'], $_POST['language']);
+                if(!$content) {
+                    // an error occured, do some error handling
+                    echo "Unable to create content";
+                } else {
+                    if(isset($_POST['moreContent'])) {
+                        // redirect to addContent-form, for now print sth
+                        echo "Successful created mew article, now creating more content";
+                    } else {
+                        // redirect somewhere, for now print sth
+                        echo "Successful created new article";
+                    }
+                }
+            }
+        } else {
+            echo "Failure";
+        }
     }
 ?>
+
