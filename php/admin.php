4f0343e6703c96c9cd7fb03cf0ce376bb6ef2cc3
KingWarin <info@kingwarin.de>
2017-02-08 19:26:49 +0100

    Admin: Add new entries
    New entries for editing existing blog posts, creating new categories and
    adding new languages

--- php/admin.php
+++ php/admin.php
@@ -8,6 +8,9 @@
         echo 'Welcome '.$_SESSION['username'];
         echo '<hr />';
         echo '<br /><a href="new_entry.php">Create a new blog post</a>';
+        echo '<br /><a href="list_entries.php">Edit an existing blog post</a>';
+        echo '<br /><a href="new_category.php">Create a new category</a>';
+        echo '<br /><a href="new_language.php">Add a new language</a>';
         echo '<hr />';
         echo '<a href="?logout">Logout</a>';
     }
