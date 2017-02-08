67e06c0229fd8ebd61b2c6598415aa866e195e47
KingWarin <info@kingwarin.de>
2017-02-08 19:28:59 +0100

    EditEntry: Enhance editing
    Enable editing existing content and adding content for another language
    at same time.

--- php/edit_entry.php
+++ php/edit_entry.php
@@ -12,6 +12,7 @@
 
     $connection = new Connection();
     $entry = $connection->getEntry($articleId);
+    $languages = $connection->getRemainingLanguagesForArticle($articleId);
 ?>
 <html>
     <head>
@@ -24,7 +25,8 @@
         <div>
             <form action="update_entry.php" method="post">
                 <label for="articleHeading">Heading:</label>
-                <input type="text" name="articleHeading" value="<?php echo $entry['heading'] ?>" />
+                <input type="text" name="articleHeading" value="<?php echo $entry['heading'] ?>" disabled/>
+                <input type="hidden" name="articleId" value="<?php echo $articleId ?>" />
                 <hr />
                 <?php
                     foreach($entry['content'] as $content) {
@@ -35,8 +37,25 @@
                     <input type="text" name="<?php echo "lang[".$content['languageId']."][language]" ?>" value="<?php echo $content['language'] ?>" disabled />
                     <label for="content">Content:</label>
                     <textarea name="<?php echo "lang[".$content['languageId']."][content]" ?>"><?php echo $content['content'] ?></textarea>
+                    <input type="hidden" name="<?php echo "lang[".$content['languageId']."][save]" ?>" value="update" />
+                    <input type="hidden" name="<?php echo "lang[".$content['languageId']."][contentId]" ?>" value="<?php echo $content['contentId'] ?>" />
                     <hr />
                 <?php
+                    }
+                    if($languages && count($languages) > 0) {
+                        foreach($languages as $language) {
+                ?>
+                            <label for="contentHeading">Language dependent heading:</label>
+                            <input type="text" name="<?php echo "lang[".$language['languageId']."][contentHeading]" ?>" />
+                            <label for="language">Language:</label>
+                            <input type="text" name="<?php echo "lang[".$language['languageId']."][language]" ?>" value="<?php echo $language['language'] ?>" disabled />
+                            <label for="content">Content:</label>
+                            <textarea name="<?php echo "lang[".$language['languageId']."][content]" ?>"></textarea>
+                            <label for="save">Save language content</label>
+                            <input type="checkbox" name="<?php echo "lang[".$language['languageId']."][save]" ?>" />
+                            <hr />
+                <?php
+                        }
                     }
                 ?>
                 <input type="submit" value="Update" />
